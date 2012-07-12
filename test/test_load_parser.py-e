ce305a4ed7f9c36de06e0b47637d8c9e2df6080e
Tristan Havelick <tristan@havelick.com>
2012-07-11 18:49:35 -0600

    added load syntax to load_parser

--- test/test_load_parser.py-e
+++ test/test_load_parser.py-e
@@ -19,6 +19,9 @@ create session with weight 4 as 'test1':
 
 create session with weight 1 as 'test2':
     get '/foo'
+
+create load:
+    spawn 1 users every 1 seconds for 1 seconds
 """)
         
         self.assertEqual(len(result.sessions), 2)
@@ -32,7 +35,37 @@ create session with weight 1 as 'test2':
         delete_action = session.actions[1]
         self.assertEqual(delete_action.method, "delete")
         self.assertEqual(delete_action.url, "/view/view1")
-                         
+
+    def test_load(self):
+        parser = LoadParser()
+        result = parser.parse("""
+create session with weight 1 as 'test1':
+    get '/foo'
+
+create load:                
+    spawn 2 users every 4 seconds for 10 minutes up to 100 users
+    spawn 5 users every 10 seconds for 50 minutes
+
+""")
+    
+        self.assertEqual(len(result.load.spawns), 2)
+        spawn1, spawn2 = result.load.spawns
+        self.assertEqual(spawn1.user_count, "2")
+        self.assertEqual(spawn1.user_time, "4")
+        self.assertEqual(spawn1.user_time_units, "seconds")
+        self.assertEqual(spawn1.max_duration, "10")
+        self.assertEqual(spawn1.max_duration_units, "minutes")
+        self.assertEqual(spawn1.max_users, "100")
+
+        self.assertEqual(spawn2.user_count, "5")
+        self.assertEqual(spawn2.user_time, "10")
+        self.assertEqual(spawn2.user_time_units, "seconds")
+        self.assertEqual(spawn2.max_duration, "50")
+        self.assertEqual(spawn2.max_duration_units, "minutes")
+        self.assertEqual(spawn2.max_users, "")
+
+
+           
 if __name__ == '__main__':
     unittest.main()
 
--- tsunami/load_parser.py
+++ tsunami/load_parser.py
@@ -1,6 +1,6 @@
 
 from pyparsing import Keyword, Word, nums, QuotedString, OneOrMore, \
-    Forward, Group
+    Forward, Group, Optional
 
 from pprint import pprint
 class LoadParser:
@@ -13,17 +13,35 @@ class LoadParser:
         url = string.setResultsName("url")
         request = Group(method + url)
         action = request
+        action_list = \
+            Group(OneOrMore(request)).setResultsName("actions")
 
-        action_list = Group( OneOrMore(request) ).setResultsName("actions")
         session = Group( Keyword("create") + \
             Keyword("session") + Keyword("with") + \
             Keyword("weight") + \
             intNum.setResultsName("weight")  + Keyword("as") + \
             string.setResultsName("name")  + \
             ":" + action_list)
-
         session_list = OneOrMore(session).setResultsName("sessions")
-        self.grammar = session_list
+        
+        time_period = Keyword("minutes") | Keyword("seconds")
+        spawn = Group( Keyword("spawn") + \
+            intNum.setResultsName("user_count") + \
+            Keyword("users") + Keyword("every") + \
+            intNum.setResultsName("user_time") + \
+            time_period.setResultsName("user_time_units") + \
+            Keyword("for") + \
+            intNum.setResultsName("max_duration") + \
+            time_period.setResultsName("max_duration_units") + \
+            Optional( Keyword("up") + Keyword("to") + \
+            intNum.setResultsName("max_users") +  Keyword("users")))
+        spawn_list = OneOrMore(spawn).setResultsName("spawns")
+        load = Group( Keyword("create") + Keyword("load") + ":" + \
+            spawn_list).setResultsName("load")
+        script = session_list + load
+       
+
+        self.grammar = script
         self.symbols = {
             'sessions': []
         }
